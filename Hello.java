114b1ebf5bd557086f583ce117af2ec6cc4728de
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2024-01-31 20:54:15 +0200

    Displaying numbers from 1 to 10

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 class HelloWorld {
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        int i=0;
+        
+    while (i < 10) {
+        System.out.println(i + 1);
+        i++;
+        
+    }
     }
 }
