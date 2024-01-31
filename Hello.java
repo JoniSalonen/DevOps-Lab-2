5d22788e610892f1d561cd48f0efde302f895861
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2024-01-31 21:00:47 +0200

    New string program

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 class HelloWorld {
     
     public static void main(String[] args) {
+        System.out.println("Welcome");
+
         int i=0;
-        
+
     while (i < 10) {
         System.out.println(i + 1);
         i++;
